8c4513202c7a936815a9a3bf2c0cccaeddb4566b
Alex <akennedy4155@gmail.com>
2018-11-20 13:32:24 -0700

    made the code for playing the snake game into its own file

---
+++ Play.js
@@ -0,0 +1,33 @@
+// <editor-fold> MAIN p5 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function setup() {
+  // setup/initialization here
+  if (width % tileSize !== 0 || height % tileSize !== 0) {
+    throw new Error('Either width or height invalid. Must be divisible evenly by the tile size.');
+  }
+
+  createCanvas(width, height);
+  g = new GameBoard(width, height, tileSize);
+  frameRate(speed);
+}
+
+function draw() {
+  g.playTurn();
+  g.draw();
+}
+
+function keyPressed() {
+  if (keyCode == 38) {
+    g.snake.changeDirection(Direction.UP);
+  } else if (keyCode == 37) {
+    g.snake.changeDirection(Direction.LEFT);
+  } else if (keyCode == 39) {
+    g.snake.changeDirection(Direction.RIGHT);
+  } else if (keyCode == 40) {
+    g.snake.changeDirection(Direction.DOWN);
+  } else if (keyCode == 32) {
+    g.snake.reset();
+  }
+}
+
+// </editor-fold> END MAIN p5~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
