2240b7dcee82a488a408424afbfd0b426d6287b9
Alex <akennedy4155@gmail.com>
2018-11-21 00:32:44 -0700

    made it all about that snake

--- models/Snek.js
+++ models/Snek.js
@@ -1,82 +1,4 @@
 //TODO ADD A Score thing in the html
-
-// <editor-fold> CONSTANTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const tileSize = 32;
-const width = 160;
-const height = 160;
-const speed = 10;
-
-const redColor = new Color(255, 0, 0);
-const greenColor = new Color(0, 255, 0);
-const whiteColor = new Color(255, 255, 255);
-const grayColor = new Color(220, 220, 220);
-
-// </editor-fold> END CONSTANTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-class GameBoard {
-
-  // creates tiles in the grid
-  constructor(width, height, tileSize) {
-    let white = true;
-    this.tiles = [];
-    for (let x = 0; x < width / tileSize; x++) {
-      let row = [];
-      (x % 2 == 1) ? white = false: white = true;
-      for (let y = 0; y < height / tileSize; y++) {
-        let color = whiteColor;
-        if (!white)
-          color = grayColor;
-        row.push(new Tile(
-          new Location(x, y),
-          tileSize,
-          color));
-        white = !white;
-      }
-      this.tiles.push(row);
-    }
-
-    // creates the snake and the food
-    this.snake = new Snake(new Location(0, 0));
-    this.food = new Food(this.snake.getFullLocation());
-  }
-
-  // all of the game logic here
-  playTurn() {
-    this.snake.move(this);
-    if (this.snake.location.equals(this.food.location)) {
-      this.snake.eat();
-      this.food = new Food(this.snake.getFullLocation());
-    }
-  }
-
-  draw() {
-    // draw all of the tiles
-    noStroke();
-    for (let row in this.tiles) {
-      for (let tile in this.tiles[row]) {
-        this.tiles[row][tile].draw();
-      }
-    }
-
-    stroke(0);
-    // draw the food and the snake
-    this.food.draw();
-    this.snake.draw();
-  }
-
-  // <editor-fold> BUTTON OPERATIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // function for the click of the start button
-  start() {
-    this.snake.playing = true;
-  }
-
-  // reset the snake to the starting position
-  reset() {
-    this.snake.respawn();
-  }
-  // </editor-fold> END BUTTON OPERATIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-}
-
 class Snake extends Tile {
 
   constructor(location) {
@@ -210,7 +132,6 @@ class Snake extends Tile {
 
   // get the locations that the snake is occupying
   getFullLocation() {
-    print([this.location].concat(this.tail.map(t => t.location)));
     return [this.location].concat(this.tail.map(t => t.location));
   }
 
@@ -241,28 +162,4 @@ class Snake extends Tile {
 
   // </editor-fold> END UTILITY ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-}
-
-class Food extends Tile {
-  constructor(snakeTiles) {
-    let valid = false;
-    let x, y, loc;
-    // keep making food until it spawns in a valid location
-    while (!valid) {
-      // pick random x and y and make a location out of it
-      x = Math.floor(Math.random() * width / tileSize);
-      y = Math.floor(Math.random() * height / tileSize);
-      loc = new Location(x, y);
-      // default valid to true
-      valid = true;
-      // check all of the tiles of the snake to see if the food is under
-      for (let i = 0; i < snakeTiles.length; i++) {
-        if (loc.equals(snakeTiles[i]))
-          valid = false;
-      }
-    }
-    // create the food
-    super(new Location(x, y), tileSize, greenColor);
-  }
 }
