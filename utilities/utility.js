09246534a397654b360a5d7d5eb4c65119c7e39d
Alex <akennedy4155@gmail.com>
2018-11-20 23:02:12 -0700

    refactored utilities

---
+++ utilities/utility.js
@@ -0,0 +1,67 @@
+// <editor-fold> UTILITIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class Color {
+
+  constructor(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+
+}
+
+let Direction = Object.freeze({
+  'UP': {
+    "x": 0,
+    "y": -1
+  },
+  'LEFT': {
+    "x": -1,
+    "y": 0
+  },
+  'RIGHT': {
+    "x": 1,
+    "y": 0
+  },
+  'DOWN': {
+    "x": 0,
+    "y": 1
+  }
+});
+
+class Location {
+
+  // x and y are defined as (0, 0) top left, x horizontal, y vertical
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  equals(otherLoc) {
+    return this.x == otherLoc.x && this.y == otherLoc.y;
+  }
+
+  add(otherLoc) {
+    return new Location(this.x + otherLoc.x, this.y + otherLoc.y);
+  }
+}
+
+class Tile {
+
+  constructor(location, size, fillColor = redColor) {
+    this.location = location;
+    this.size = size;
+    this.fillColor = fillColor;
+  }
+
+
+  draw() {
+    fill(this.fillColor.r, this.fillColor.g, this.fillColor.b);
+    rect(this.location.x * this.size,
+      this.location.y * this.size,
+      this.size,
+      this.size
+    );
+  }
+
+}
+// </editor-fold> END UTILITIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
