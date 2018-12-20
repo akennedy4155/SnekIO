bb11214a707b7ea9a51c8f431879b275ed48a76a
Alex-Kennedy-Laptop <akennedy4155@gmail.com>
2018-12-20 09:32:04 -0700

    added a colliding behavior to the location that checks if the location is colliding with the snake or a wall

--- utilities/utility.js
+++ utilities/utility.js
@@ -12,19 +12,27 @@ class Color {
 let Direction = Object.freeze({
   'UP': {
     "x": 0,
-    "y": -1
+    "y": -1,
+    'l': 'LEFT',
+    'r': 'RIGHT'
   },
   'LEFT': {
     "x": -1,
-    "y": 0
+    "y": 0,
+    'l' : 'DOWN',
+    'r' : 'UP'
   },
   'RIGHT': {
     "x": 1,
-    "y": 0
+    "y": 0,
+    'l' : 'UP',
+    'r' : 'DOWN'
   },
   'DOWN': {
     "x": 0,
-    "y": 1
+    "y": 1,
+    'l' : 'RIGHT',
+    'r' : 'LEFT'
   }
 });
 
@@ -49,6 +57,24 @@ class Location {
       let xDist = otherLoc.x - this.x;
       let yDist = otherLoc.y - this.y;
       return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+  }
+
+  // determines if the location is intersecting a wall or another part of the snake
+  colliding(snake, width, height) {
+    let isCollision = false;
+    // if snake's head is off the screen then collision
+    if (this.x < 0 || this.x >= (width / tileSize) ||
+       this.y < 0 || this.y >= (height / tileSize)) {
+      isCollision = true;
+    }
+    // if head collides with any other part of the tail then collision
+    for (let i = 0; i < snake.tail.length; i++) {
+      if (this.equals(snake.tail[i].location)) {
+        isCollision = true;
+        break;
+      }
+    }
+    return isCollision;
   }
 }
 
